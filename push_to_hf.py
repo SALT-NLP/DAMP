96a6d653c2c962c55a17d2f88916c4915f5c712f
Helw150 <wbh230@nyu.edu>
2022-11-22 13:38:07 -0500

    Add Parsing Training!

--- push_to_hf.py
+++ push_to_hf.py
@@ -1,6 +1,13 @@
 from datasets import load_dataset
 
 
+def fix_headers(examples):
+    new_examples = {}
+    for header in examples.keys():
+        new_examples[header.strip()] = examples[header]
+    return new_examples
+
+
 langs = ["en", "de", "es", "fr", "hi", "th"]
 splits = ["eval", "test", "train"]
 split_dict = {}
@@ -9,7 +16,7 @@ for lang in langs:
         split_dict[f"{split}_{lang}"] = f"./mtop/{lang}/{split}.txt.csv"
 
 d = load_dataset("csv", data_files=split_dict)
-d.push_to_hub("WillHeld/mtop")
+d.map(fix_headers).push_to_hub("WillHeld/mtop")
 
 splits = ["validation", "test", "train"]
 split_dict = {}
@@ -17,7 +24,7 @@ for split in splits:
     split_dict[f"{split}"] = f"./cstop/{split}.tsv.csv"
 
 d = load_dataset("csv", data_files=split_dict)
-d.push_to_hub("WillHeld/hinglish_top")
+d.map(fix_headers).push_to_hub("WillHeld/hinglish_top")
 
 splits = ["eval", "test", "train"]
 domains = [
@@ -34,18 +41,18 @@ split_dict = {}
 for split in splits:
     split_dict[split] = [f"./topv2/{domain}_{split}.tsv.csv" for domain in domains]
 d = load_dataset("csv", data_files=split_dict)
-d.push_to_hub("WillHeld/top_v2")
+d.map(fix_headers).push_to_hub("WillHeld/top_v2")
 
 splits = ["eval", "test", "train"]
 split_dict = {}
 for split in splits:
     split_dict[split] = f"./CSTOP/CSTOP_{split}.tsv.csv"
 d = load_dataset("csv", data_files=split_dict)
-d.push_to_hub("WillHeld/cstop")
+d.map(fix_headers).push_to_hub("WillHeld/cstop")
 
 splits = ["eval", "test", "train"]
 split_dict = {}
 for split in splits:
     split_dict[split] = f"./cstop_artificial/{split}.csv"
 d = load_dataset("csv", data_files=split_dict)
-d.push_to_hub("WillHeld/cstop_artificial")
+d.map(fix_headers).push_to_hub("WillHeld/cstop_artificial")
---
+++ train_t5_base.sh
@@ -0,0 +1,29 @@
+export CUDA_VISIBLE_DEVICES=2,3
+
+# More Accumulation because model parallel instead of data parallel
+for DATASET in mtop top_v2 cstop_artificial
+do
+    python run_parsing.py \
+	   --model_name_or_path google/mt5-base \
+	   --do_train \
+	   --do_eval \
+	   --do_predict \
+	   --learning_rate 1e-3 \
+	   --weight_decay 0.0 \
+	   --max_steps 3000 \
+	   --evaluation_strategy "steps" \
+	   --logging_steps 200 \
+	   --eval_steps 200 \
+	   --save_steps 200 \
+	   --save_total_limit 1 \
+	   --load_best_model_at_end True \
+	   --dataset_name $DATASET \
+	   --push_to_hub_model_id t5-base-vanilla-$DATASET \
+	   --push_to_hub True \
+	   --output_dir /data/wheld3/mt5-base-$DATASET \
+	   --per_device_train_batch_size=8 \
+	   --gradient_accumulation_steps=64\ 
+	   --per_device_eval_batch_size=8 \
+	   --overwrite_output_dir \
+	   --predict_with_generate
+done
