42a835daf236d8adfe636bb064858277b025b864
Helw150 <wbh230@nyu.edu>
2022-12-10 13:17:03 -0500

    Pointer Adv Eval

---
+++ eval_t5_pointer_adv.sh
@@ -0,0 +1,51 @@
+export CUDA_VISIBLE_DEVICES=6,7
+
+DATASET="mtop"
+for LANG in en es fr de hi th
+do
+    python run_parsing.py \
+	   --model_name_or_path WillHeld/t5-small-pointer-adv-$DATASET \
+	   --do_predict \
+	   --lang $LANG \
+	   --output_dir /data/wheld3/mt5-small-pointer-adv-$DATASET/eval-$LANG \
+	   --per_device_eval_batch_size=16 \
+	   --dataset_name $DATASET \
+	   --pointer_method \
+	   --predict_with_generate
+done
+
+python run_parsing.py \
+       --model_name_or_path WillHeld/t5-small-pointer-adv-top_v2\
+       --do_predict \
+       --output_dir /data/wheld3/mt5-small-pointer-adv-top_v2/eval-hinglish \
+       --per_device_eval_batch_size=16 \
+       --dataset_name hinglish_top \
+       --pointer_method \
+       --predict_with_generate
+
+python run_parsing.py \
+       --model_name_or_path WillHeld/t5-small-pointer-adv-top_v2\
+       --do_predict \
+       --output_dir /data/wheld3/mt5-small-pointer-adv-top_v2/eval-en \
+       --per_device_eval_batch_size=16 \
+       --dataset_name top_v2 \
+       --pointer_method \
+       --predict_with_generate
+
+python run_parsing.py \
+       --model_name_or_path WillHeld/t5-small-pointer-adv-cstop_artificial\
+       --do_predict \
+       --output_dir /data/wheld3/mt5-small-pointer-adv-cstop_artificial/eval-spanglish \
+       --per_device_eval_batch_size=32 \
+       --dataset_name cstop \
+       --pointer_method \
+       --predict_with_generate
+
+python run_parsing.py \
+       --model_name_or_path WillHeld/t5-small-pointer-adv-cstop_artificial\
+       --do_predict \
+       --output_dir /data/wheld3/mt5-small-pointer-adv-cstop_artificial/eval-en \
+       --per_device_eval_batch_size=32 \
+       --dataset_name cstop_artificial \
+       --pointer_method \
+       --predict_with_generate
